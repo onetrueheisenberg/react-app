5b4050d21db5fff0dc21478b080389d4d26ede0d
Sundara Subramanian Swaminathan <sundarasubramanian@Vikrams-MacBook.local>
2023-07-01 20:31:33 +0530

    adding effect sample code

---
+++ src/ProductList.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const ProductList = ({ category }: { category: string }) => {
+  const [products, setProducts] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching Products in", category);
+    setProducts(["clothing", "household"]);
+  }, [category]);
+  return <div>ProductList</div>;
+};
+
+export default ProductList;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import ExpenseTrackerApp from "./ExpenseTrackerApp.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ExpenseTrackerApp />
-    {/* <App /> */}
+    {/* <ExpenseTrackerApp /> */}
+    <App />
   </React.StrictMode>
 );
